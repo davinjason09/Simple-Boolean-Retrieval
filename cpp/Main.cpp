31baeb4fc44692c96b16d0614880545ba074d253
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2024-09-10 17:30:47 +0700

    feat(Main): added logging, comparison between optimized and unoptimized retrieval

--- cpp/Main.cpp
+++ cpp/Main.cpp
@@ -1,15 +1,23 @@
 #include "BooleanRetrieval.h"
 #include "InvertedIndex.h"
 #include <atomic>
+#include <chrono>
 #include <fstream>
 #include <iostream>
 #include <thread>
 
 std::atomic<bool> indexReady(false);
+double indexTime;
+double queryTime;
+double optimizedQueryTime;
 
 void buildIndex(InvertedIndex &index, const std::string &filePath,
                 const std::string &delimiter) {
+  auto start = std::chrono::high_resolution_clock::now();
   index.ReadCSV(filePath, delimiter);
+  auto end = std::chrono::high_resolution_clock::now();
+
+  indexTime = std::chrono::duration<double>(end - start).count();
   indexReady = true;
 }
 
@@ -17,7 +25,7 @@ int main(int argc, char *argv[]) {
   InvertedIndex index;
 
   std::ofstream fout("output.txt");
-  std::string filePath = "News.csv";
+  std::string filePath = "../data/News.csv";
   std::string delimiter = ",";
 
   std::thread indexThread(buildIndex, std::ref(index), filePath, delimiter);
@@ -28,7 +36,7 @@ int main(int argc, char *argv[]) {
   std::cout << "Enter query: ";
   getline(std::cin, query);
 
-  std::vector<std::string> queryArray = model.ParseQuery(query);
+  std::vector<std::string> tokenArray = model.ParseQuery(query);
 
   if (!indexReady)
     std::cout << "Building index...\n";
@@ -36,20 +44,45 @@ int main(int argc, char *argv[]) {
   indexThread.join();
 
   std::unordered_map<std::string, std::vector<int>> docIDs = index.GetDocIDs();
-  fout << "Dict size:" << docIDs.size() << '\n';
 
-  for (auto &it : docIDs)
-    fout << "'" << it.first << "' : " << it.second.size() << '\n';
+  for (auto &it : docIDs) {
+    fout << it.first << ": { ";
+    for (int id : it.second)
+      fout << id << ", ";
+    fout << "}\n";
+  }
 
-  Node *root = model.BuildTree(docIDs, queryArray, false);
+  auto start = std::chrono::high_resolution_clock::now();
+  Node *root = model.BuildTree(tokenArray, index, false);
+  std::vector<int> result = model.CalculateTree(root, index.totalDocuments);
+  auto end = std::chrono::high_resolution_clock::now();
+  queryTime = std::chrono::duration<double>(end - start).count();
+
+  start = std::chrono::high_resolution_clock::now();
+  Node *rootOptimized = model.BuildTree(tokenArray, index, true);
+  std::vector<int> resultOptimized =
+      model.CalculateTree(root, index.totalDocuments);
+  end = std::chrono::high_resolution_clock::now();
+  optimizedQueryTime = std::chrono::duration<double>(end - start).count();
 
   std::cout << '\n';
+  std::cout << "Tree:\n";
   model.PrintTree(root);
-  std::vector<int> result = model.CalculateTree(root, docIDs.size());
 
-  std::cout << "\ndocID: {  " << (result.size() ? '\b' : ' ');
-  for (int id : result)
-    std::cout << id << ", ";
+  std::cout << "\nOptimized tree:\n";
+  model.PrintTree(rootOptimized);
 
-  std::cout << "\b\b }\n";
+  std::cout << "\nAmount: " << result.size() << '\n';
+  /*std::cout << "\ndocID: {  ";*/
+  /*for (int id : result)*/
+  /*  std::cout << id << ", ";*/
+  /**/
+  /*std::cout << "}\n";*/
+
+  std::cout << "\n\nStatistics:\n";
+  std::cout << "Indexing time: " << indexTime << "s\n";
+  std::cout << "Total Documents: " << index.totalDocuments << '\n';
+  std::cout << "Total Terms: " << docIDs.size() << '\n';
+  std::cout << "Query time: " << queryTime << "s\n";
+  std::cout << "Optimized query time: " << optimizedQueryTime << "s\n";
 }
